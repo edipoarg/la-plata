d69f5429dfb745e3391b779d3178917262b09eab
alexaiconejo <fondo.gif>
2024-05-07 17:09:37 -0300

    autorxs urls and image ok

--- src/components/Investigaciones/autorxs/Autorxs.jsx
+++ src/components/Investigaciones/autorxs/Autorxs.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import styles from "./Autorxs.module.css";
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import styles from './Autorxs.module.css';
 
 const Autorxs = () => {
   const [autorxsData, setAutorxsData] = useState([]);
@@ -8,13 +8,11 @@ const Autorxs = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(
-          "../src/components/investigaciones/autorxs/autorxs.json",
-        ); // Ajusta la ruta según tu estructura
+        const response = await fetch('../src/components/investigaciones/autorxs/autorxs.json');
         const data = await response.json();
         setAutorxsData(data);
       } catch (error) {
-        console.error("Error al cargar datos:", error);
+        console.error('Error al cargar datos:', error);
       }
     };
 
@@ -22,28 +20,26 @@ const Autorxs = () => {
   }, []);
 
   return (
-    <>
-      <section className={styles.container}>
-        <section className={styles.titleContainer}>
-          <h2 className={styles.title}>Autorxs</h2>
-        </section>
-        <section className={styles.autorxsContainer}>
-          {autorxsData.map((autorx, index) => (
-            <div className={styles.autorContainer} key={index}>
-              <img
-                src={autorx.imagen}
-                alt={`Foto de ${autorx.nombre}`}
-                className={styles.autorImagen}
-              />
-              <h2 className={styles.autorNombre}>{autorx.nombre}</h2>
-              <Link to={autorx.enlaceVer} className={styles.verEnlace}>
-                Ver
-              </Link>
-            </div>
-          ))}
-        </section>
+    <section className={styles.container}>
+      <section className={styles.titleContainer}>
+        <h2 className={styles.title}>Autorxs</h2>
       </section>
-    </>
+      <section className={styles.autorxsContainer}>
+        {autorxsData.map((autorx, index) => (
+          <div className={styles.autorContainer} key={index}>
+            <img
+              src={autorx.imagen}
+              alt={`Foto de ${autorx.nombre}`}
+              className={styles.autorImagen}
+            />
+            <h2 className={styles.autorNombre}>{autorx.nombre}</h2>
+            <Link to={autorx.enlaceVer} className={styles.verEnlace}>
+              Ver
+            </Link>
+          </div>
+        ))}
+      </section>
+    </section>
   );
 };
 
