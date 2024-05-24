4263626407959661719603b5f6500f98806c9a93
Lidiador <alex.ai.conejo@gmail.com>
2024-05-23 22:58:20 -0300

    reportes y gatillo markers ok

--- public/data/index.js
+++ public/data/index.js
@@ -5,6 +5,8 @@ import departamentosLaPlata from "../data/departamentosLaPlata.json";
 import laPlata from "../data/laPlata.json";
 import dependenciasCaba from "../data/dependenciasCaba.json"
 import  gatillo  from "../data/gatilloCaba.json";
+import  reportes  from "../data/reportesCaba.json";
 
 
-export {  departamentos, caba, barriosCaba, laPlata, departamentosLaPlata, gatillo, dependenciasCaba };
+
+export {  departamentos, caba, barriosCaba, laPlata, departamentosLaPlata, gatillo, reportes, dependenciasCaba };
