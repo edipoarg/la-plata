d035e7fdc3582805e50a94952beb61d7adb0acef
Lidiador <alex.ai.conejo@gmail.com>
2024-05-26 12:30:01 -0300

    mensaje de proximamente no va, sale la web directo, deshice ccambios en routers y app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 /*import Mapa from "./components/Mapa";*/
 import "./App.css";
 
+import Mapa from "./components/Mapa";
+
 const App = () => {
   return (
     <>
-      <section className="testContainer">
-        <img id="logo" className="logo" src="favicon.png" alt="" />
-        <h3 className="test">Próximamente</h3>
-        <h3 className="nueva">Nueva Web</h3>
-      </section>
+      <Mapa></Mapa>
     </>
   );
 };
--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
-/*import Navbar from "../components/navbar/Navbar.jsx";*/
+import Navbar from "../components/navbar/Navbar.jsx";
 import styles from "./Root.module.css";
 
 export default function Root() {
   return (
     <>
       <section className={styles.root}>
-        {/*<Navbar />  Navbar siempre se muestra */}
+        <Navbar />
         <Outlet />
       </section>
     </>
