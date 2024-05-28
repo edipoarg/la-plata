9e70fafbbe9656064b70b65c97e02595a6f9cc28
Juani Garay <jngaray93@gmail.com>
2024-05-27 21:47:25 -0300

    make report link link to report

--- src/components/Investigaciones/Investigaciones.jsx
+++ src/components/Investigaciones/Investigaciones.jsx
@@ -6,13 +6,13 @@ import Mapa from "../Mapa";
 
 const tituloNotaPrincipal =
   "La bonaerense en la mira: de una internación psiquiátrica a un doble femicidio";
-const fechaUltimoReporte = "27/09/2023";
+const fechaUltimoReporte = "08/05/2024";
 const fotoNotaPrincipal =
   "https://static.wixstatic.com/media/0f4ca0_8720002caf4347e986b0913169d7bfdc~mv2.jpg/v1/fill/w_1211,h_630,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Crisis_salud_Mental_Mapa3.jpg";
 const fechaNotaPrincipal = "08/05/2024";
 const autorxsNotaPrincipal = " Julia Pascolini";
 const ilusNotaPrincipal = "Panchopepe";
-const numeroReporte = "Tercer";
+const numeroReporte = "Cuarto";
 
 const Investigaciones = () => {
   return (
@@ -70,11 +70,11 @@ const Investigaciones = () => {
             </section>
 
             <section className={styles.investCol2}>
-              <section
+              <Link
+                to="/reportes"
                 style={Temas.reportesTema}
                 className={styles.ultimoReporteContainer}
               >
-                {/* QUE ONDA ESTO */}
                 <img src="" alt="" className={styles.fotoUltimoReporte} />
                 <section className={styles.datosReporte}>
                   <h4 className={styles.fechaUltimoReporte}>
@@ -87,7 +87,7 @@ const Investigaciones = () => {
                     de violencia policial
                   </h4>
                 </section>
-              </section>
+              </Link>
               <a
                 href="https://open.spotify.com/show/1fhXtCulH39aZgv9P7WH7k"
                 style={Temas.investTema}
