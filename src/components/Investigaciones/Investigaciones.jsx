663b447f4ba1b8b89de26f9585a944b1e4e56ca8
anon author <noemail@nothing.null>
2024-08-11 14:45:27 -0300

    investigaciones.json in public and the first La Plata investigation added

--- src/components/Investigaciones/Investigaciones.jsx
+++ src/components/Investigaciones/Investigaciones.jsx
@@ -1,18 +1,17 @@
 import styles from "./Investigaciones.module.css";
-import Temas from "../Temas";
 import { Link } from "react-router-dom";
 import Icons from "../iconos/Icons";
 import Mapa from "../Mapa";
 
 const tituloNotaPrincipal =
-  "La bonaerense en la mira: de una internación psiquiátrica a un doble femicidio";
-const fechaUltimoReporte = "27/09/2023";
+  "La bonaerense en la mira: de una internación psiquiátrica a un doble demicidio";
+const fechaUltimoReporte = "11/08/2024";
 const fotoNotaPrincipal =
   "https://static.wixstatic.com/media/0f4ca0_8720002caf4347e986b0913169d7bfdc~mv2.jpg/v1/fill/w_1211,h_630,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Crisis_salud_Mental_Mapa3.jpg";
-const fechaNotaPrincipal = "08/05/2024";
-const autorxsNotaPrincipal = " Julia Pascolini";
-const ilusNotaPrincipal = "Panchopepe";
-const numeroReporte = "Tercer";
+const fechaNotaPrincipal = "29/03/2022";
+const autorxsNotaPrincipal = "Julia Pascolini";
+const ilusNotaPrincipal = "Brenda Greco";
+const numeroReporte = "Cuarto";
 
 const Investigaciones = () => {
   return (
@@ -26,18 +25,12 @@ const Investigaciones = () => {
               className={styles.headerIcon}
               iconSize="4rem"
             />
-
-            <section className={styles.typeMenu}>
-              <h3 className={styles.type}>Informes</h3>
-              <h3 className={styles.type}>Detrás del expediente</h3>
-              <h3 className={styles.type}>Reportes</h3>
-            </section>
           </section>
 
           <section className={styles.investigaciones}>
             <section className={styles.investNovedades}>
               <Link to="/investigacion/bonaerense-mira">
-                <section className={styles.notaPrincipal}>
+                <article className={styles.notaPrincipal}>
                   <img
                     src={fotoNotaPrincipal}
                     alt=""
@@ -65,15 +58,22 @@ const Investigaciones = () => {
                       {fechaNotaPrincipal}
                     </h6>
                   </section>
+                </article>
+              </Link>
+              <Link to="/lista" className={styles.todasLinkContainer}>
+                <section className={styles.todasLink}>
+                  <Icons
+                    icon={"flecha"}
+                    className={styles.linkIcon}
+                    iconSize="2rem"
+                  />
+                  <h2 className={styles.flecha}>Más investigaciones</h2>
                 </section>
               </Link>
             </section>
 
             <section className={styles.investCol2}>
-              <section
-                style={Temas.reportesTema}
-                className={styles.ultimoReporteContainer}
-              >
+              <Link to="/reportes" className={styles.ultimoReporteContainer}>
                 <img src="" alt="" className={styles.fotoUltimoReporte} />
                 <section className={styles.datosReporte}>
                   <h4 className={styles.fechaUltimoReporte}>
@@ -86,45 +86,27 @@ const Investigaciones = () => {
                     de violencia policial
                   </h4>
                 </section>
-              </section>
+              </Link>
               <a
                 href="https://open.spotify.com/show/1fhXtCulH39aZgv9P7WH7k"
-                style={Temas.investTema}
                 className={styles.podcastContainer}
               >
-                <section style={Temas.investTema} className={styles.podcast}>
+                <section className={styles.podcast}>
+                  <Icons
+                    icon={"podcast"}
+                    className={styles.podcastIcon}
+                    iconSize="2.8rem"
+                  />
                   <h2 className={styles.tituloPodcast}>Podcast</h2>
-                  <div className={styles.bajadaPodcast}>
-                    <h5 className={styles.play}>ir</h5>
-                  </div>
+                  <div className={styles.bajadaPodcast}></div>
                 </section>
               </a>
-              <section className={styles.investMore}>
-                <Link
-                  to="/autorxs"
-                  style={Temas.investTema}
-                  className={styles.autorxsSectionContainer}
-                >
-                  <section
-                    style={Temas.investTema}
-                    className={styles.autorxsSection}
-                  >
-                    <h2 className={styles.tituloAutorxsSection}>Autorxs</h2>
-                    <div></div>
-                  </section>
-                </Link>
-                <Link
-                  to="/investigaciones/lista"
-                  className={styles.todasLinkContainer}
-                >
-                  <section
-                    style={Temas.investTema}
-                    className={styles.todasLink}
-                  >
-                    <h5 className={styles.flecha}>Todas</h5>
-                  </section>
-                </Link>
-              </section>
+              <Link to="/autorxs" className={styles.autorxsSectionContainer}>
+                <section className={styles.autorxsSection}>
+                  <h2 className={styles.tituloAutorxsSection}>Autorxs</h2>
+                  <div></div>
+                </section>
+              </Link>
             </section>
           </section>
         </section>
